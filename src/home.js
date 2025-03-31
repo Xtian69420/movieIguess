6d6aff6d54fd4823ae19e68902606e4f3f6e7297
Christian Signo <150314113+Xtian69420@users.noreply.github.com>
2025-03-31 13:58:27 +0800

    Update home.js

--- src/home.js
+++ src/home.js
@@ -1,6 +1,7 @@
 async function Home() {
     const API_KEY = '97df57ffd9278a37bc12191e00332053';
     const container = document.querySelector('.movie-container');
+    const container1 = document.querySelector('.series-container')
     const slider = document.querySelector('.backdrop-slider');
 
     if (!container) {
@@ -28,6 +29,7 @@ async function Home() {
             const card = document.createElement('div');
             card.className = 'movie-card';
 
+
             card.innerHTML = `
                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title || movie.name}" class="movie-poster">
                 <h2 class="movie-title">${movie.title || movie.name}</h2>
@@ -45,6 +47,40 @@ async function Home() {
     } catch (error) {
         console.error('Error fetching data:', error);
     }
+
+    try {
+        const response = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}`);
+        const data = await response.json();
+
+        console.log(data)
+
+        updateBackdrop();
+        setInterval(updateBackdrop, 3000);
+
+        data.results.forEach(movie => {
+            const card1 = document.createElement('div');
+            card1.className = 'series-card'
+
+            card1.innerHTML = `
+                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title || movie.name}" class="series-poster">
+                <h2 class="series-title">${movie.title || movie.name}</h2>
+                <p class="series-overview">${movie.overview ? movie.overview.substring(0, 100) + '...' : 'No overview available.'}</p>
+                <p class="series-release-date"><strong>Release Date:</strong> ${movie.release_date || movie.first_air_date || 'N/A'}</p>
+                <p class="series-rating"><strong>Rating:</strong> ${movie.vote_average || 'N/A'} / 10</p>
+            `;
+
+            card1.addEventListener('click', () => {
+                window.location.href = `viewMovie.html?movieId=${movie.id}&type=tv`;
+            });
+            if(movie.poster_path){
+                container1.appendChild(card1);
+            }
+
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+    
 }
 
 Home();
